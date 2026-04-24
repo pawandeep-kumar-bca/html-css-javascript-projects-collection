a9ac4f6c62f311415cd96cb16e00fb47633c6e45
pawandeep kumar <pawandeepkumar7634@gmail.com>
2026-04-24 20:31:16 +0530

    feat: add toast notification/snackbar

---
+++ 31-toast-notification/app.js
@@ -0,0 +1,21 @@
+let toastBox = document.getElementById("toastBox");
+let successMsg = `<i class="ri-checkbox-circle-fill"></i>Successfully submitted`;
+let errorMsg = `<i class="ri-close-circle-fill"></i>Please fix the error!`;
+let invalidMsg = `<i class="ri-error-warning-fill"></i>Invalid input , check again`;
+function showToast(msg) {
+  let toast = document.createElement("div");
+  toast.classList.add("toast");
+  toast.innerHTML = msg;
+  toastBox.appendChild(toast);
+
+  if(msg.includes('error')){
+    toast.classList.add("error");
+  }
+   if(msg.includes('Invalid')){
+    toast.classList.add("Invalid");
+  }
+
+  setTimeout(()=>{
+    toast.remove();
+  },6000)
+}
